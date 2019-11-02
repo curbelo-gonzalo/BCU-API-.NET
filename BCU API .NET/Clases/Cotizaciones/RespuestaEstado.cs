e8b853f07e108e0ee6d473f4e757c09e3a6d4d7b
curbelo-gonzalo <32030204+curbelo-gonzalo@users.noreply.github.com>
2019-11-02 01:34:44 -0300

    Agregada clase RespuestaEstado.

---
+++ BCU API .NET/Clases/Cotizaciones/RespuestaEstado.cs
@@ -0,0 +1,17 @@
+﻿using BcuApiDotNet.uy.gub.bcu.cotizaciones;
+
+namespace BcuApiDotNet {
+    public class RespuestaEstado {
+        public readonly CodigoError Codigo;
+        public readonly EstadoRespuesta Status;
+        public readonly string Mensaje;
+
+        internal RespuestaEstado(respuestastatus rs)
+            : this((EstadoRespuesta)rs.status, (CodigoError)rs.codigoerror, rs.mensaje) { }
+        public RespuestaEstado(EstadoRespuesta status, CodigoError codigo, string mensaje) {
+            this.Status = status;
+            this.Codigo = codigo;
+            this.Mensaje = mensaje;
+        }
+    }
+}
