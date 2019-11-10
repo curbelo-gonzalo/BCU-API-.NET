bb5ce929ad1151dcea0c845b17cbc7cfd6222b1c
curbelo-gonzalo <32030204+curbelo-gonzalo@users.noreply.github.com>
2019-11-10 17:11:44 -0300

    Agregada clase OpcionesConsultaMoneda.

---
+++ BCU API .NET/Clases/Monedas/OpcionesConsultaMoneda.cs
@@ -0,0 +1,11 @@
+﻿namespace BcuApiDotNet.Monedas {
+    public class OpcionesConsultaMoneda {
+        public OpcionesConsultaMoneda() { }
+        public OpcionesConsultaMoneda(GrupoMoneda grupo) {
+            this.Grupo = grupo;
+        }
+
+
+        public GrupoMoneda Grupo { get; set; }
+    }
+}
