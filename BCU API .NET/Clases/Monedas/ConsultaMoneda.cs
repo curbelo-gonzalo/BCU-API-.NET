32a6762acbd4c52773b8d40f2b07125a08604072
curbelo-gonzalo <32030204+curbelo-gonzalo@users.noreply.github.com>
2019-11-10 17:42:51 -0300

    Agregada clase ConsultaMoneda.
    La implementación actual solo contiene métodos de ejecución síncronos.

---
+++ BCU API .NET/Clases/Monedas/ConsultaMoneda.cs
@@ -0,0 +1,39 @@
+﻿using BcuApiDotNet.uy.gub.bcu.monedas;
+using System;
+
+namespace BcuApiDotNet.Monedas {
+    public class ConsultaMoneda: IDisposable {
+        private readonly wsbcumonedas _wsbcu_mone = new wsbcumonedas();
+
+
+        public string URL {
+            get {
+                return _wsbcu_mone.Url;
+            }
+            set {
+                _wsbcu_mone.Url = value;
+            }
+        }
+        public bool UseDefaultCredentials {
+            get {
+                return _wsbcu_mone.UseDefaultCredentials;
+            }
+            set {
+                _wsbcu_mone.UseDefaultCredentials = value;
+            }
+        }
+
+
+        public void Dispose() {
+            _wsbcu_mone.Dispose();
+        }
+
+
+        public ResultadoConsultaMoneda Execute(OpcionesConsultaMoneda entrada) {
+            if(entrada is null) {
+                throw new ArgumentNullException(nameof(entrada));
+            }
+            return new ResultadoConsultaMoneda(_wsbcu_mone.Execute(entrada._wsbcu_in));
+        }
+    }
+}
