948d54e7fbcb7c8e9f59521387bb3eacd5eca268
curbelo-gonzalo <32030204+curbelo-gonzalo@users.noreply.github.com>
2019-11-10 17:35:37 -0300

    Cambiada implementación de OpcionesConsultaMoneda.

--- BCU API .NET/Clases/Monedas/OpcionesConsultaMoneda.cs
+++ BCU API .NET/Clases/Monedas/OpcionesConsultaMoneda.cs
@@ -1,11 +1,23 @@
-﻿namespace BcuApiDotNet.Monedas {
+﻿using BcuApiDotNet.uy.gub.bcu.monedas;
+
+namespace BcuApiDotNet.Monedas {
     public class OpcionesConsultaMoneda {
+        internal readonly wsmonedasin _wsbcu_in = new wsmonedasin();
+
+
         public OpcionesConsultaMoneda() { }
         public OpcionesConsultaMoneda(GrupoMoneda grupo) {
             this.Grupo = grupo;
         }
 
 
-        public GrupoMoneda Grupo { get; set; }
+        public GrupoMoneda Grupo {
+            get {
+                return (GrupoMoneda)_wsbcu_in.Grupo;
+            }
+            set {
+                _wsbcu_in.Grupo = (sbyte)value;
+            }
+        }
     }
 }
