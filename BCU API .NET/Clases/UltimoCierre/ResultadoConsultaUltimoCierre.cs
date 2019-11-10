55986a4bfac1ceedfdd9c5df4cbc214b77a616dc
curbelo-gonzalo <32030204+curbelo-gonzalo@users.noreply.github.com>
2019-11-10 18:18:00 -0300

    Agregada clase ResultadoConsultaUltimoCierre.

---
+++ BCU API .NET/Clases/UltimoCierre/ResultadoConsultaUltimoCierre.cs
@@ -0,0 +1,20 @@
+﻿using BcuApiDotNet.uy.gub.bcu.ultimocierre;
+using System;
+
+namespace BcuApiDotNet.UltimoCierre {
+    public class ResultadoConsultaUltimoCierre {
+        private readonly wsultimocierreout _wsbcu_out;
+
+
+        internal ResultadoConsultaUltimoCierre(wsultimocierreout wsbcu_out) {
+            _wsbcu_out = wsbcu_out;
+        }
+
+
+        public DateTime? Fecha {
+            get {
+                return _wsbcu_out.Fecha;
+            }
+        }
+    }
+}
